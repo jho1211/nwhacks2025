2b82f64ff1db927b4d6c83f7019e1f066a68e57c
Jeff <jho1211@students.cs.ubc.ca>
2025-01-18 13:50:30 -0800

    classes

---
+++ helping-hands-web-app/app/models/user.ts
@@ -0,0 +1,40 @@
+export interface User {
+    uid: string,
+    first_name: string,
+    last_name: string,
+    bio: string,
+    profile_img: string,
+    role: string // can be one of "mentor" or "mentee"
+}
+
+export interface Wish {
+    item_name: string,
+    url: string,
+    priority: number
+}
+
+export interface Wishlist {
+    uid: string,
+    wishlist: Wish[]
+}
+
+export interface Message {
+    id: string,
+    sender_id: string,
+    status: string,
+    content: string,
+    timestamp: Date,
+    receiver_id: string
+}
+
+export interface Connection {
+    id: string,
+    participants: User[],
+    status: string,
+    messages_id: string
+}
+
+export interface Messages {
+    id: string,
+    messages: Message[]
+}
