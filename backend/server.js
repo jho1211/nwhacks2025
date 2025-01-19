9946202cae44c7d49f1c8bba8d97afacbb253fd5
Victor Yu <victor7wlyu@gmail.com>
2025-01-18 22:03:30 -0800

    updateUserInfo, updateWishlist, updateConnectionsList

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,10 @@ var app = express()
  
 app.use(cors(), express.json({'limit': '5mb'}));
 
-import { getUserInfo, getMentees, addUser, getWishlist, getConnections, createWishlist, addNewConnectionsList } from './fetch.js'
+import { getUserInfo, getMentees, addUser, getWishlist,
+         getConnections, createWishlist, addNewConnectionsList,
+         updateUserInfo, updateWishlist, updateConnectionsList,
+         } from './fetch.js'
 
 // GET route to send user information with specified user id to frontend
 app.get('/api/user/:uid', getUserInfo); 
@@ -24,20 +27,20 @@ app.post('/api/user', addUser);
 // POST route to create wishlist
 app.post('/api/wishlist', createWishlist);
 
-// // POST route to add new connections list
+// POST route to add new connections list
 app.post('/api/connections', addNewConnectionsList);
 
-// // PUT route to update user information
-// app.put('/api/user', fetchHandlers.updateUserInfo);
+// PUT route to update user information
+app.put('/api/user', updateUserInfo);
 
-// // PUT route to update wishlist for a specified user
-// app.put('/api/wishlist', fetchHandlers.updateWishlist);
+// PUT route to update wishlist for a specified user
+app.put('/api/wishlist', updateWishlist);
 
-// // PUT route to update connections list for a specified user
-// app.put('/api/connections', fetchHandlers.updateConnectionsList);
+// PUT route to update connections list for a specified user
+app.put('/api/connections', updateConnectionsList);
 
 // // DELETE route to remove a user by user ID from database
-// app.delete('/api/user/:uid', fetchHandlers.removeUser);
+// app.delete('/api/user/:uid', removeUser);
 
 // // DELETE route to remove the connections list for a user from database
 // app.delete('/api/connections/:uid', fetchHandlers.removeConnectionsList);
