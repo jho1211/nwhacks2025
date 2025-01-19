2044bd36ac31af86755821a314b94cc699116dce
Jeff <jho1211@students.cs.ubc.ca>
2025-01-19 11:24:13 -0800

    add/remove from wishlist

--- helping-hands-web-app/app/user/[uid]/page.tsx
+++ helping-hands-web-app/app/user/[uid]/page.tsx
@@ -91,9 +91,9 @@ export default function ProfilePage({params}: {params: Promise<{ uid: string }>}
                         <div>{user?.bio}</div>
                     </div>
                 </div>
-                {user?.role == "mentee" ? <div>
+                {user?.role == "mentee" ? <div className="wishlist-container">
                     <div className="wishlist-header">Wishlist</div>
-                    <WishlistTable wishlist={wishlist}></WishlistTable>
+                    <WishlistTable wishlist={wishlist} canEdit={curUid.current == user?.uid}></WishlistTable>
                 </div> : null}
                 {curUid.current == user?.uid ? <ConnectionsTable connections={connections} loggedUid={uid}></ConnectionsTable> : <button className="connect-button" onClick={handleConnect}>CONNECT WITH {user?.name}</button>}
             </div>
