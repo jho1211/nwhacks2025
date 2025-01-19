ed90c2fa8c50dfe08174135b5a3807c5fc0016af
Jeff <jho1211@students.cs.ubc.ca>
2025-01-19 04:09:19 -0800

    implement ConnectionsRow in profile page

---
+++ helping-hands-web-app/app/components/ConnectionRow/ConnectionRow.tsx
@@ -0,0 +1,57 @@
+"use client"
+
+import { Connection, User } from "@/app/models/models"
+import { useState, useEffect } from 'react'
+import Link from 'next/link'
+import axios from "@/app/utils/axios_instance";
+import "./styles.css"
+import { useRouter } from "next/navigation";
+
+export default function ConnectionRow(props: {uid: string, conn: Connection}) {
+    const [user, setUser] = useState<User>();
+    const router = useRouter();
+
+    useEffect(() => {
+        props.conn.participants[0] == props.uid ? 
+        axios.get(`user/${props.conn.participants[1]}`).then(resp => setUser(resp.data)) :
+        axios.get(`user/${props.conn.participants[0]}`).then(resp => setUser(resp.data))
+    }, [])
+
+    const handleAccept = (other: string) => {
+        const newConn: Connection = {
+            id: props.conn.id,
+            messagesId: props.conn.messagesId,
+            participants: props.conn.participants,
+            status: "accepted"
+        };
+        props.conn.status = "accepted";
+        axios.put("/connections", newConn).then(() => alert(`Accepted the connection with ${other}`));
+        router.refresh();
+    }
+
+    const handleReject = (other: string) => {
+        const newConn: Connection = {
+            id: props.conn.id,
+            messagesId: props.conn.messagesId,
+            participants: props.conn.participants,
+            status: "rejected"
+        };
+        props.conn.status = "rejected";
+        axios.put("/connections", newConn).then(() => alert(`Rejected the connection with ${other}`));
+        router.refresh();
+    }
+    
+    return (user ? <div className="conn">
+        <div className="connection-row-profile">
+            <img className="connection-row-profile-img" src={user.profile_img}></img>
+            <div className="connection-row-profile-name">{user.name}</div>
+        </div>
+        {props.conn.status == "pending" ? 
+        <div className="connection-row-actions">
+            <div className="connection-row-action" onClick={() => handleAccept(user.name)}>✅</div>
+            <div className="connection-row-action" onClick={() => handleReject(user.name)}>❌</div>
+        </div> : 
+        props.conn.status == "accepted" ? <Link href="/chat"><button className="connection-row-chat-btn">CHAT</button></Link> : 
+        <div className="connection-row-rejected-text">REJECTED</div>}
+    </div> : null)
+}
---
+++ helping-hands-web-app/app/components/ConnectionsTable/ConnectionsTable.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import { User, Connection } from "@/app/models/models";
+import "./styles.css"
+import ConnectionRow from "../ConnectionRow/ConnectionRow";
+
+export default function ConnectionsTable(props: {connections: Connection[], loggedUid: string}) {
+    return (
+    <div>
+        <div className="connections-table-header">My Connections</div>
+        <div className="connections-table">
+            {props.connections.map((conn, idx) => <ConnectionRow key={idx} conn={conn} uid={props.loggedUid}></ConnectionRow>)}
+        </div>
+    </div>)
+}
