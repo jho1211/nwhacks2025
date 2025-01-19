227a50262d0c3f517ddea2c91f345e9a8dfe577a
Jeff <jho1211@students.cs.ubc.ca>
2025-01-19 00:10:47 -0800

    add navbar to components

---
+++ helping-hands-web-app/app/components/Navbar/Navbar.tsx
@@ -0,0 +1,43 @@
+import Link from "next/link";
+import styles from "./style.module.css";
+
+export default function Navbar() {
+    return (
+        <header>
+    <div className="logo">
+    <img src="/helpinghandlogo.png" alt="Logo"/>
+    </div>
+    <nav className={styles.navbar}>
+        <div className={styles.navbar__container}>
+            <ul className={styles.navbar__menu}>
+                <li className={styles.navbar__item}>
+                    <a href="/" className="navbar__links">
+                    Home
+                    </a>
+                </li>
+                <li className={styles.navbar__item}>
+                    <a href="/tech.html" className="navbar__links">
+                    About
+                    </a>
+                </li>
+                <li className={styles.navbar__item}>
+                    <a href="/connect" className="navbar__links">
+                    Profiles
+                    </a>
+                </li>
+                <li className={styles.navbar__item}>
+                    <a href="/" className="navbar__links">
+                    Testimonies
+                    </a>
+                </li>
+                <Link href="user/signin" className="button">
+                  <li className={styles.navbar__btn}>
+                    Sign In
+                  </li>
+                </Link>
+            </ul>
+        </div>
+    </nav>
+  </header>
+    )
+}
