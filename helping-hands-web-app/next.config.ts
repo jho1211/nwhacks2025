14ab51f4f75b596483f7af498b9e739fdef2d8f7
Jeff <jho1211@students.cs.ubc.ca>
2025-01-19 18:06:17 -0800

    update nextjs proxy

--- helping-hands-web-app/next.config.ts
+++ helping-hands-web-app/next.config.ts
@@ -14,7 +14,15 @@ const nextConfig: NextConfig = {
             hostname: 'static.vecteezy.com',
         }
     ]
-}
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://helping-hand-backend.vercel.app/:path*', // Replace with your backend domain
+      },
+    ];
+  },
 };
 
 export default nextConfig;
