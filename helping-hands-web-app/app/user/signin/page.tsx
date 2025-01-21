d8eb375a768e60c95b75173461791f0deb23be7e
Jeff <jho1211@students.cs.ubc.ca>
2025-01-20 22:45:11 -0800

    fix signin page triggering multiple GET requests

--- helping-hands-web-app/app/user/signin/page.tsx
+++ helping-hands-web-app/app/user/signin/page.tsx
@@ -6,13 +6,9 @@ import { useState, FormEvent } from "react";
 import {
   getAuth,
   signInWithEmailAndPassword,
-  onAuthStateChanged,
 } from "firebase/auth";
 import "./styles.css";
 import Link from "next/link";
-import { userStore } from "@/store/user";
-import { type User } from "@/app/models/models";
-import axios from "@/app/utils/axios_instance";
 import Image from "next/image";
 
 export default function Register() {
@@ -20,17 +16,8 @@ export default function Register() {
   const [password, setPassword] = useState<string>("");
   const [error, setError] = useState<string>("");
   const router = useRouter();
-  const setUser = userStore((state) => state.setUser);
 
   const auth = getAuth(firebaseApp);
-
-  onAuthStateChanged(auth, (user) => {
-    if (user) {
-      router.push(`/user/${user.uid}`);
-    } else {
-      return;
-    }
-  });
 
   async function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
@@ -39,9 +26,7 @@ export default function Register() {
     signInWithEmailAndPassword(auth, email, password)
       .then(async (user) => {
         const uid = user.user.uid;
-        console.log(`Your uid is: ${uid}`);
-        const userData = await axios.get<User>(`user/${uid}`);
-        setUser(userData.data);
+        router.push(`${uid}`);
       })
       .catch((err) => setError(err.message));
   }
